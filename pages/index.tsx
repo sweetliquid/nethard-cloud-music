c6284cab964a1688b2bfb4b89997398dc3cd716d
Sweet Liquid <himejisyana@hotmail.com>
2020-12-12 17:13:33 +0800

    No reason for commit junk code, lol

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,94 @@
+import classNames from 'classnames'
 import Head from 'next/head'
+import { FC, useCallback } from 'react'
+
+function hover(...classes: string[]) {
+  return classes.map(c => `hover:${c}`).join(' ')
+}
+
+/**
+ * I tried to write some shit code, hahhhhh.
+ */
+const NavigationButton: FC<{
+  direction: '<' | '>'
+  className?: string
+}> = ({
+  direction,
+  className = classNames(
+    'cursor-pointer w-6 text-sm',
+    hover('bg-red-700', 'text-red-800', 'border-red-900')
+  ),
+}) => (
+  <a
+    className={className}
+    onClick={useCallback(() => {
+      switch (direction) {
+        case '<':
+          // TODO: backward route
+          break
+        case '>':
+          // TODO: forward route
+          break
+        default:
+          break
+      }
+    }, [])}
+  >
+    {direction}
+  </a>
+)
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Nethard Cloud Music</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main className="full-screen flex flex-col">
+        <div className="bg-red-600 h-12 flex-center-y">
+          <div className="flex-center-y w-48 px-1">
+            <img
+              className="w-10 h-10"
+              src="/NetEase_Music-Logo.wine.svg"
+              alt="logo"
+            />
+            <span className="text-white">网易云音乐</span>
+          </div>
 
-      <footer></footer>
+          <div
+            className={classNames(
+              'flex',
+              'border border-red-700 rounded',
+              'text-white text-center font-bold'
+            )}
+          >
+            <NavigationButton direction="<" />
+            <div className="border-l border-red-700" />
+            <NavigationButton direction=">" />
+          </div>
+
+          <div className="flex-center-y ml-3">
+            <input
+              className={classNames(
+                'rounded-xl py-1 px-2',
+                'bg-red-700 outline-none',
+                'text-xs text-white',
+                // TODO: placeholder active variants: opacity-0
+                'placeholder-gray-300 placeholder-opacity-60 hover:placeholder-opacity-0'
+              )}
+              type="text"
+              placeholder="搜索音乐，歌手，歌词，用户"
+            />
+          </div>
+        </div>
+        <div className="flex-auto flex">
+          <div className="w-48 bg-gray-100"></div>
+          <div className="flex-auto"></div>
+        </div>
+        <div className="h-10"></div>
+      </main>
     </div>
   )
 }
