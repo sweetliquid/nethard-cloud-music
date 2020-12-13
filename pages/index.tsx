4d9df3a757d59056ef1260b40e06bc44ad769be2
Sweet Liquid <himejisyana@hotmail.com>
2020-12-13 16:14:47 +0800

    Make the navbar not so bad

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
 import Head from 'next/head'
 import { FC, FormEvent, useCallback } from 'react'
-import { BiSearch } from 'react-icons/bi'
+import { BiSearch, BiCaretDown } from 'react-icons/bi'
+import { FaRegUserCircle } from 'react-icons/fa'
+import { RiTShirtLine, RiSettings3Line } from 'react-icons/ri'
+import {
+  VscMail,
+  VscChromeMinimize,
+  VscChromeMaximize,
+  VscChromeClose,
+} from 'react-icons/vsc'
 
 /**
  * I tried to write some shit code, hahhhhh.
@@ -41,12 +49,12 @@ const SearchBar: FC = () => {
     <form className="flex-center-y" onSubmit={handleSubmit}>
       <input
         // TODO: placeholder active variants: opacity-0
-        className="px-3 py-1 text-xs text-white placeholder-gray-300 bg-red-700 outline-none rounded-xl w-52 placeholder-opacity-60 hover:placeholder-opacity-0"
+        className="px-3 py-1 text-xs text-white placeholder-gray-300 bg-red-700 outline-none rounded-xl placeholder-opacity-60 hover:placeholder-opacity-0 w-60"
         type="text"
         placeholder="搜索音乐，歌手，歌词，用户"
       />
       <button>
-        <BiSearch className="relative text-gray-300 -left-5 opacity-70 hover:text-white" />
+        <BiSearch className="relative text-gray-300 -left-6 opacity-70 hover:text-white" />
       </button>
     </form>
   )
@@ -80,7 +88,31 @@ export default function Home() {
           <div className="ml-3">
             <SearchBar />
           </div>
+
+          {/* TODO: Replace this icon with an avatar */}
+          <FaRegUserCircle className="ml-auto text-xl text-white cursor-pointer" />
+          <div className="flex border-r border-red-700">
+            <div className="flex items-center gap-1 px-3 text-xs text-gray-300 cursor-pointer hover:text-white">
+              <p className="">Sweet Liquid</p>
+              <BiCaretDown className="inline" />
+            </div>
+            <div className="flex items-center gap-1 px-3 text-lg text-gray-300 cursor-pointer hover:text-white">
+              <RiTShirtLine />
+            </div>
+            <div className="flex items-center gap-1 px-3 text-lg text-gray-300 cursor-pointer hover:text-white">
+              <VscMail />
+            </div>
+            <div className="flex items-center gap-1 px-3 text-lg text-gray-300 cursor-pointer hover:text-white">
+              <RiSettings3Line />
+            </div>
+          </div>
+          <div className="flex px-3">
+            <VscChromeMinimize className="w-6 text-lg text-gray-300 cursor-pointer hover:text-white" />
+            <VscChromeMaximize className="w-6 text-lg text-gray-300 cursor-pointer hover:text-white" />
+            <VscChromeClose className="w-6 text-lg text-gray-300 cursor-pointer hover:text-white" />
+          </div>
         </div>
+
         <div className="flex flex-auto">
           <div className="w-48 bg-gray-100"></div>
           <div className="flex-auto"></div>
